a80be157fbbf6f58986daa19c4b85ea84b6982e9
Ray Ashir <rayeed.ashir@gmail.com>
2021-11-20 11:23:04 -0800

    uncomment db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
-// const mongoose = require("mongoose");
-// const config = require("config");
-// const db = config.get("mongoURI");
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(db, {
-//             useCreateIndex: true,
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             useFindAndModify: false
-//         });
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
 
-//         console.log('MongoDB connected..');
-//     }catch(err){
-//         console.error(err.message);
-//         // Exit process with failure
-//         process.exit(1)
-//     }
-// }
+        console.log('MongoDB connected..');
+    }catch(err){
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1)
+    }
+}
 
-// module.exports = connectDB;
+module.exports = connectDB;
