8f37261979367a692f12c61326c911158e85fec1
Ray Ashir <rayeed.ashir@gmail.com>
2021-11-24 23:09:58 -0800

    update applicant model

--- server/models/applicant.js
+++ server/models/applicant.js
@@ -3,17 +3,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ApplicantSchema = new Schema({
-  name: String,
-  //todo the posting Should be Posting model not just a string
-  posting: String, 
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  posting: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }, 
   // status can be inprogress, rejected, offer
-  status: String,
-  // schedule should be a schedule model object not a string
-  schedule: String,
+  status: {
+    type: String,
+    required: true, 
+    trim: true
+  },
+  schedule: {
+    type: Schema.Types.ObjectId,
+    ref: 'Schedule',
+  },
   // since we are only considering an applicant for one job, we can have the feedback added here
-  feedback: String, 
+  feedback: {
+    type: String, 
+    trim: true,
+  },
   // todo add email validation 
-  email: String
+  email: {
+    type: String, 
+    required: true, 
+    trim: true,
+  }
 });
 
 const Applicant = mongoose.model("Applicant", ApplicantSchema);
