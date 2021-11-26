12f8a625c317c1e550c4e2df9209236daa7abee7
Zakk Fast <zakkfastpro@gmail.com>
2021-11-26 09:30:12 -0800

    created footer component

---
+++ client/src/Components/layout/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer class="footer has-background-primary has-text-centered">
+      <div class="columns">
+          <div class="column">
+              <ul class="has-text-dark">
+                  <li><a href="#" class="is-size-5 has-text-danger">Zapplicant</a></li>
+                  <li>Bussiness Solutions</li>
+                  <li>Mobile App</li>
+                  <li>Careers</li>
+              </ul>
+          </div>
+          <div class="column">
+            <ul class="has-text-dark">
+                <li><a href="#" class="is-size-5 has-text-danger">About Us</a></li>
+                <li>Contact Us</li>
+                <li>Leadership</li>
+                <li>Investors</li>
+            </ul>
+        </div>
+        <div class="column">
+            <ul class="has-text-dark">
+                <li><a href="#" class="is-size-5 has-text-danger">Community</a></li>
+                <li>Events</li>
+                <li>Locations</li>
+                <li>News</li>
+            </ul>
+        </div>
+      </div>
+    </footer>
+    )
+}
