69c878ff5d00501056305a23a61b1af3f6c32565
Zakk Fast <zakkfastpro@gmail.com>
2021-11-27 15:06:25 -0800

    git is being stupid again..

--- client/src/Components/ApplicantView.js/ApplicantCard.js
+++ client/src/Components/ApplicantView.js/ApplicantCard.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export const ApplicantCard = () => {
-    return (
-        <div>
-            <h1>
-                Hello
-            </h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
--- client/src/Components/layout/Footer.js
+++ client/src/Components/layout/Footer.js
@@ -1,34 +1,46 @@
-import React from 'react'
+import React from 'react';
 
 export const Footer = () => {
-    return (
-        <footer className="footer has-background-primary has-text-centered">
-      <div className="columns">
-          <div className="column">
-              <ul className="has-text-dark">
-                  <li><a href="/" className="is-size-5 has-text-danger">Zapplicant</a></li>
-                  <li>Bussiness Solutions</li>
-                  <li>Mobile App</li>
-                  <li>Careers</li>
-              </ul>
-          </div>
-          <div className="column">
-            <ul className="has-text-dark">
-                <li><a href="/" className="is-size-5 has-text-danger">About Us</a></li>
-                <li>Contact Us</li>
-                <li>Leadership</li>
-                <li>Investors</li>
-            </ul>
+  return (
+    <footer className='footer has-background-primary has-text-centered'>
+      <div className='columns'>
+        <div className='column'>
+          <ul className='has-text-dark'>
+            <li>
+              <a href='/' className='is-size-5 has-text-danger'>
+                Zapplicant
+              </a>
+            </li>
+            <li>Bussiness Solutions</li>
+            <li>Mobile App</li>
+            <li>Careers</li>
+          </ul>
         </div>
-        <div className="column">
-            <ul className="has-text-dark">
-                <li><a href="/" className="is-size-5 has-text-danger">Community</a></li>
-                <li>Events</li>
-                <li>Locations</li>
-                <li>News</li>
-            </ul>
+        <div className='column'>
+          <ul className='has-text-dark'>
+            <li>
+              <a href='/' className='is-size-5 has-text-danger'>
+                About Us
+              </a>
+            </li>
+            <li>Contact Us</li>
+            <li>Leadership</li>
+            <li>Investors</li>
+          </ul>
+        </div>
+        <div className='column'>
+          <ul className='has-text-dark'>
+            <li>
+              <a href='/' className='is-size-5 has-text-danger'>
+                Community
+              </a>
+            </li>
+            <li>Events</li>
+            <li>Locations</li>
+            <li>News</li>
+          </ul>
         </div>
       </div>
     </footer>
-    )
-}
+  );
+};
