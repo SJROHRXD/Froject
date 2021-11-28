094d32ad70e7922f20488ee5f63dd3c3588a994f
Ray Ashir <rayeed.ashir@gmail.com>
2021-11-28 11:10:44 -0800

    fix applicantByName and Email to add schedule and posting

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,10 +7,10 @@ const resolvers = {
     },
     applicantByEmail: async (parent, args) => {
     // returns the first found record that matches the regex email ignoring the case
-    return await Applicant.findOne({email:{'$regex' : args.email, '$options' : 'i'}});
+    return await Applicant.findOne({email:{'$regex' : args.email, '$options' : 'i'}}).populate('posting').populate('schedule');
     },
     applicantByName: async (parent, args) => {
-      return await Applicant.findOne({name:{'$regex' : args.name, '$options' : 'i'}});
+      return await Applicant.findOne({name:{'$regex' : args.name, '$options' : 'i'}}).populate('posting').populate('schedule');
     },
     employees: async () => {
       return await Employee.find({});
