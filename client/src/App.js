a7a18d831ba7c4860dbbfdf8bb46482f1c97f768
Ray Ashir <rayeed.ashir@gmail.com>
2021-11-28 21:32:44 -0800

    updated react to integrate with apollo/graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 
 import { Navbar } from './Components/layout/Navbar';
 import { Footer } from './Components/layout/Footer';
@@ -8,18 +10,25 @@ import { Footer } from './Components/layout/Footer';
 import { SignIn } from './Components/Landing/SignIn.js';
 import { Applicant } from './Components/ApplicantView.js/Applicant';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={SignIn()} />
-          <Route exact path='/applicant' element={Applicant()} />
-        </Routes>
-        <Footer />
-      </Fragment>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={SignIn()} />
+            <Route exact path='/applicant' element={Applicant()} />
+          </Routes>
+          <Footer />
+        </Fragment>
+      </Router>
+      </ApolloProvider>
   );
 }
 
