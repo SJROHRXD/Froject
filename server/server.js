05edd8601877953dc9924cb27fcb37cadcb80cdd
Ray Ashir <rayeed.ashir@gmail.com>
2021-12-01 22:40:44 -0800

    fremove unneeded routing

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 // Adding Required Dependencies
 const express = require("express");
-const path = require('path');
-const db = require('./config/db')
+const path = require("path");
+const db = require("./config/db");
 
 // GraphQL declarations
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 const server = new ApolloServer({
-  typeDefs, 
-  resolvers
+  typeDefs,
+  resolvers,
 });
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,25 +19,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 server.applyMiddleware({ app });
 
-
 // routes
-app.get("/", (req, res) => res.send("API Running"));
-app.get("/schedule", (req, res) => res.send("API Running"));
-app.get("/comment", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
+// app.get("/schedule", (req, res) => res.send("API Running"));
+// app.get("/comment", (req, res) => res.send("API Running"));
 // TODO: add more routes as we need them here
 
 // if we're in production, define the folder that will be used for static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`API server running on port ${PORT}!`));
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
