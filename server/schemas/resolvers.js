c4dd6256e31fbc62574f8a86cd5f24f66fb7c28a
Ray Ashir <rayeed.ashir@gmail.com>
2021-12-02 16:21:52 -0800

    update resolvers for jwt

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,6 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { Applicant, Employee, Post, Schedule } = require('../models');
-const { signToken } = require('../utils/auth');
-
+const { AuthenticationError } = require("apollo-server-express");
+const { Applicant, Employee, Post, Schedule } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -26,9 +25,9 @@ const resolvers = {
     employees: async () => {
       return await Employee.find({});
     },
-    employee: async (parent, {employeeId}) => {
+    employee: async (parent, { employeeId }) => {
       return Employee.findOne({ _id: employeeId });
-    }, 
+    },
     posts: async () => {
       return await Post.find({});
     },
@@ -37,31 +36,34 @@ const resolvers = {
     },
   },
   Mutation: {
-    addApplicant: async (parent, {name, email, status}) => {
-      return await Applicant.create({name, email, status});
-    }, 
-    addSchedule: async (parent, {date}) => {
-      return await Schedule.create({date});
-    }, 
-    addEmployee: async (parent, {name, title, password}) => {
-      return await Employee.create({name, title, password});
+    addApplicant: async (parent, { name, email, status }) => {
+      return await Applicant.create({ name, email, status });
+    },
+    addSchedule: async (parent, { date }) => {
+      return await Schedule.create({ date });
+    },
+    addEmployee: async (parent, { name, title, password }) => {
+      const employee = await Employee.create({ name, title, password });
+      const token = signToken(employee);
+      return { token, user };
     },
     login: async (parent, { name, password }) => {
       const employee = await Employee.findOne({ name });
-
+      console.log(`employee ${name} and password ${password}`);
       if (!employee) {
-        throw new AuthenticationError('No employee with this password found!');
+        throw new AuthenticationError("No employee with this password found!");
       }
 
       const correctPw = await employee.isCorrectPassword(password);
+      console.log(`inside login mutaion... correctPw ${correctPw}`);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect password!');
+        throw new AuthenticationError("Incorrect password!");
       }
 
       const token = signToken(employee);
       return { token, employee };
-    }, 
+    },
 
     addPost: async (parent, { name }) => {
       return await Post.create({ name });
